3066ccd6a270bc691e07b3a8d0de76a7e4cc43c7
junehyukyoo <yoo.junehyuk@gmail.com>
2021-03-21 13:42:28 -0500

    Accidentally committed most of work to the week 1 branch. This is now the week 2 branch.

--- include/gas_container.h
+++ include/gas_container.h
@@ -12,9 +12,8 @@ namespace idealgas {
  */
 class GasContainer {
  public:
-  /**
-   * TODO: Add more parameters to this constructor, and add documentation.
-   */
+  GasContainer(int num_particles);
+
   GasContainer();
 
   /**
@@ -28,12 +27,71 @@ class GasContainer {
    */
   void AdvanceOneFrame();
 
- private:
   /**
-   * This variable is just for the purposes of demonstrating how to make a shape move
-   * across a screen. Please remove it once you start working on your code.
+   * Checks if any of the particles are about to hit the container. If so,
+   * change its velocity the moment it hits a wall.
    */
-  int dummy_variable_ = 0;
+  void CheckWallContact();
+
+  /**
+   * Checks if any particles are colliding with each other. If so, change the
+   * velocity of colliding particles.
+   */
+  void CheckParticleCollision();
+
+  /**
+   * Checks if two particles are colliding.
+   * @param p1 The first particle to compare.
+   * @param p2 The second particle to compare.
+   * @return A boolean value.
+   */
+  bool ParticlesAreColliding(std::pair<vec2, vec2> p1, std::pair<vec2, vec2> p2);
+
+  /**
+   * Changes the velocities of colliding particles.
+   * @param p1 The first particle.
+   * @param p2 The second particle.
+   * @return New velocity of p1 and p2 as a pair in that order.
+   */
+  std::pair<vec2, vec2> CalculateVelocityAfterCollision(std::pair<vec2, vec2> p1, std::pair<vec2, vec2> p2);
+
+  /**
+   * Generates a particle with a random position in the container and random
+   * velocity.
+   * @return A pair, the first being the position vector, the second being the
+   * velocity vector.
+   */
+  std::pair<glm::vec2, glm::vec2> GenerateRandomParticle();
+
+  /**
+   * Generates a random double from a lower and upper bound.
+   * @param lower_bound is the lower bound.
+   * @param upper_bound is the upper bound.
+   * @return A random double within the bounds.
+   */
+  double GenerateRandomDouble(double lower_bound, double upper_bound) const;
+
+  /**
+   * Returns a random color for a particle.
+   * @return Random color.
+   */
+  ci::Color RandomColorPicker() const;
+
+  /**
+   * Gets the list of all particles in the container for testing purposes.
+   * @return the list of all particles.
+   */
+  std::vector<std::pair<glm::vec2, glm::vec2>> GetParticles();
+
+  void AddParticles(std::pair<glm::vec2, glm::vec2> particle);
+
+ private:
+  std::vector<std::pair<glm::vec2, glm::vec2>> particles_;
+  int kRadius = 10;
+  vec2 kFirstPoint = vec2(100, 100);
+  vec2 kSecondPoint = vec2(600, 600);
+  ci::Color kBorderColor = ci::Color("white");
+  std::vector<ci::Color> kPossibleColors = {ci::Color("orange"), ci::Color("red"), ci::Color("green"), ci::Color("blue")};
 };
 
 }  // namespace idealgas
--- src/gas_container.cc
+++ src/gas_container.cc
@@ -1,23 +1,123 @@
 #include "gas_container.h"
+#include <random>
 
 namespace idealgas {
 
 using glm::vec2;
 
-GasContainer::GasContainer() {
-
+GasContainer::GasContainer(int num_particles) {
+  for (size_t i = 0; i < num_particles; i++) {
+    particles_.push_back(GenerateRandomParticle());
+  }
 }
+
+GasContainer::GasContainer() {}
 
 void GasContainer::Display() const {
   // This function has a lot of magic numbers; be sure to design your code in a way that avoids this.
-  ci::gl::color(ci::Color("orange"));
-  ci::gl::drawSolidCircle(vec2(dummy_variable_, 200), 10);
-  ci::gl::color(ci::Color("white"));
-  ci::gl::drawStrokedRect(ci::Rectf(vec2(100, 100), vec2(600, 400)));
+  for (const auto& particle : particles_) {
+    ci::gl::color(ci::Color(RandomColorPicker()));
+    ci::gl::drawSolidCircle(particle.first, kRadius);
+  }
+  ci::gl::color(ci::Color(kBorderColor));
+  ci::gl::drawStrokedRect(ci::Rectf(kFirstPoint, kSecondPoint));
 }
 
 void GasContainer::AdvanceOneFrame() {
-  ++dummy_variable_;
+   CheckWallContact();
+   CheckParticleCollision();
+}
+
+void GasContainer::CheckWallContact() {
+  for (std::pair<vec2, vec2> &particle : particles_) {
+    vec2 position = particle.first;
+    vec2 velocity = particle.second;
+    if ((position.x <= kFirstPoint.x + kRadius || position.x >= kSecondPoint.x - kRadius) &&
+        (position.y <= kFirstPoint.y + kRadius || position.y >= kSecondPoint.y - kRadius)) {
+      particle.second = vec2(-1 * velocity.x, -1 * velocity.y);
+      particle.first += particle.second;
+    } else if (position.x <= kFirstPoint.x + kRadius || position.x >= kSecondPoint.x - kRadius) {
+      particle.second = vec2(-1 * velocity.x, velocity.y);
+      particle.first += particle.second;
+    } else if (position.y <= kFirstPoint.y + kRadius || position.y >= kSecondPoint.y - kRadius) {
+      particle.second = vec2(velocity.x, -1 * velocity.y);
+      particle.first += particle.second;
+    } else {
+      particle.first += particle.second;
+    }
+  }
+}
+
+void GasContainer::CheckParticleCollision() {
+  for (std::pair<vec2, vec2> &particle : particles_) {
+    for (std::pair<vec2, vec2> &particle_compare : particles_) {
+      if (ParticlesAreColliding(particle, particle_compare)) {
+        std::pair<vec2, vec2> new_v = CalculateVelocityAfterCollision(particle, particle_compare);
+        particle.second = new_v.first;
+        particle_compare.second = new_v.second;
+      }
+    }
+  }
+}
+
+bool GasContainer::ParticlesAreColliding(std::pair<vec2, vec2> p1, std::pair<vec2, vec2> p2) {
+  bool collides = false;
+  bool go_same_dir = false;
+  if (std::abs(glm::distance(p1.first, p2.first)) <= 2 * kRadius) {
+    collides = true;
+  }
+  if (glm::dot((p1.second - p2.second),(p1.first - p2.first)) < 0) {
+    go_same_dir = true;
+  }
+  if (collides && go_same_dir) {
+    return true;
+  } else {
+    return false;
+  }
+}
+
+std::pair<vec2, vec2> GasContainer::CalculateVelocityAfterCollision(std::pair<vec2, vec2> p1, std::pair<vec2, vec2> p2) {
+  float dot_product_p1 = glm::dot((p1.first - p2.first),(p1.second - p2.second));
+  double magnitude_p1 = static_cast<float>(std::pow(glm::length(p1.first - p2.first),2));
+  float dot_product_over_magnitude_p1 = dot_product_p1 / magnitude_p1;
+  vec2 right_side_p1 = dot_product_over_magnitude_p1 * (p1.first - p2.first);
+  vec2 final_v_p1 = p1.second - right_side_p1;
+
+  float dot_product_p2 = glm::dot((p2.first - p1.first),(p2.second - p1.second));
+  float magnitude_p2 = static_cast<float>(std::pow(glm::length(p2.first - p1.first),2));
+  float dot_product_over_magnitude_p2 = dot_product_p2 / magnitude_p2;
+  vec2 right_side_p2 = dot_product_over_magnitude_p2 * (p2.first - p1.first);
+  vec2 final_v_p2 = p2.second - right_side_p2;
+
+  return {final_v_p1, final_v_p2};
+}
+
+std::pair<glm::vec2, glm::vec2> GasContainer::GenerateRandomParticle() {
+  vec2 position = vec2(GenerateRandomDouble(kFirstPoint.x + kRadius, kSecondPoint.x - kRadius),
+                       GenerateRandomDouble(kFirstPoint.y + kRadius, kSecondPoint.y - kRadius));
+  vec2 velocity = vec2(GenerateRandomDouble(1, kRadius/2),
+                       GenerateRandomDouble(1, kRadius/2));
+  return {position, velocity};
+}
+
+//code derived from: https://www.delftstack.com/howto/cpp/how-to-generate-random-doubles-cpp/
+double GasContainer::GenerateRandomDouble(double lower_bound, double upper_bound) const {
+  std::random_device rd;
+  std::default_random_engine eng(rd());
+  std::uniform_real_distribution<double> distribution(lower_bound, upper_bound);
+  return distribution(eng);
+}
+
+ci::Color GasContainer::RandomColorPicker() const {
+  return kPossibleColors[static_cast<int>(GenerateRandomDouble(0, kPossibleColors.size()))];
+}
+
+std::vector<std::pair<glm::vec2, glm::vec2>> GasContainer::GetParticles() {
+  return particles_;
+}
+
+void GasContainer::AddParticles(std::pair<glm::vec2, glm::vec2> particle) {
+  particles_.push_back(particle);
 }
 
 }  // namespace idealgas
